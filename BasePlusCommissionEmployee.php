a824f738391a9593ba0fcee3699ac7335a5880c8
Awura-Adwoaaa <constancemensah05@gmail.com>
2025-02-06 23:50:34 -1200

    added new file in php

---
+++ BasePlusCommissionEmployee.php
@@ -0,0 +1,33 @@
+<?php
+class BasePlusCommissionEmployee extends CommissionEmployee{
+    private float $baseSalary;
+
+    public function __construct(string $firstName, string $lastName, string $socialSecurityNumber, float $grossSales, float $commisionRate, float $baseSalary){
+        parent::__construct( $firstName, $lastName, $socialSecurityNumber, $grossSales, $commissionRate);
+        $this->setBaseSalary($baseSalary);
+    }
+
+    public function getBaseSalary(): float{
+        return $this->baseSalary;
+    }
+
+    public function setBaseSalary(float $newSalary): void{
+        if ($newSalary <= 0.0){
+            throw new InvalidArgumentException ("Base salary must be greater than 0.oo");
+        }else{
+            $this->baseSalary = $newSalary;
+        }
+    }
+
+    public function earnings(): float {
+        return $this->baseSalary + parent::earnings();
+    }
+
+    public function displayEmployeeDetails(): void {
+        parent::displayEmployeeDetails();
+        echo "Base Salary: {$this->baseSalary}\n";
+        echo "Total Earnings: {$this->earnings()}\n";
+       
+    }
+}
+?>
